e7f5d9a788263b359c267866e6c9f16b8363fa72
Daniel Markbreiter <dmarkbreiter@nhm.org>
2023-12-18 18:33:50 -0800

    change to main

--- main.py
+++ main.py
@@ -1,56 +1,46 @@
-import svgwrite
-import geopandas
-from dotenv import dotenv_values
+from factory import map_factory
 
-file_path = dotenv_values(".env")["FILE_PATH"]
-
-gdf = geopandas.read_file(f'{file_path}/north-america/north-america.json')
-gdf.to_crs(gdf.crs, '9820').plot()
-gdf.plot()
-
-scale = 100
-minx, miny, maxx, maxy = [x * scale for x in gdf.total_bounds]
-width, height = maxx - minx, maxy - miny
-
-dwg = svgwrite.Drawing(
-    f'{file_path}/north-america/states.svg',
-    #size=(width, height),
-    height='100%',
-    width='100%',
-    viewBox=f'0 {0 - (maxy-miny)} {maxx - minx} {maxy - miny}'
+'''
+map_factory(
+    basepath='./data/north-america',
+    epsg='epsg:2163',
+    layers=[
+        {
+            'name': 'countries',
+            'gdf':  'countries.geojson'
+        },
+        {
+            'name': 'usa-states',
+            'gdf': 'usa-states.geojson'
+        },
+        {
+            'name': 'mx-states',
+            'gdf': 'mexico-states.geojson'
+        },
+        {
+            'name': 'ca-provinces',
+            'gdf': 'canada-provinces.geojson'
+        },
+    ],
+    mask='north-america.geojson',
 )
 
-def translate(coord: float, type: str):
-    if type == 'x':
-        return (coord * scale) - minx
-    if type == 'y':
-        return (0-(coord * scale - miny))
+map_factory(
+    basepath='./data/africa',
+    epsg='esri:102022',
+    layers=[
+        {
+            'name': 'countries',
+            'gdf': 'african-country-boundaries.geojson'
+        }
+    ],
+    mask='africa-continent-boundary.geojson',
+)
+'''
 
-
-
-def add_path(coordinates: list):
-    path_data = "M" + " ".join([f"{translate(x, 'x')},{translate(y, 'y')}" for x, y in coordinates]) + "Z"
-    path = dwg.path(
-        d=path_data,
-        fill='none',
-        stroke='black',
-        class_='path'
-    )
-    #path.scale(scale)
-    # path.scale(1,-1)
-    dwg.add(path)
-
-
-for feature in gdf.iterrows():
-    geometry = feature[1]['geometry']
-    if geometry.geom_type == 'Polygon':
-        coordinates = geometry.exterior.coords
-        add_path(coordinates)
-
-    if geometry.geom_type == 'MultiPolygon':
-        coordinates_list = [list(x.exterior.coords) for x in geometry.geoms]
-        for coordinates in coordinates_list:
-            add_path(coordinates)
-
-dwg.add_stylesheet('map.css', title='styles')
-dwg.save()
+map_factory(
+    basepath='./data/africa',
+    epsg='esri:102022',
+    layers=[],
+    mask='africa-continent-boundary.geojson',
+)
--- map.py
+++ map.py
@@ -1,60 +1,43 @@
+import os.path
+import shapely
 import svgwrite
 import geopandas
-from dotenv import dotenv_values
-from os import listdir
-from os.path import isfile, join
-
-
-style = '''
-    g {
-      stroke-linejoin: bevel;
-      fill: none;
-    }
-    
-    path { 
-      stroke: #000000;
-      stroke-width: 1000px; 
-    }
-    
-    .example-ranges {
-      fill: #43AF6E;
-    }
-    
-    .latin-american-boundaries { 
-       stroke-width: 2000px
-     }
-     
-     
-'''
 
 
 class Map:
-    def __init__(self, name:str, scale:int=1, epsg:str='2163'):
+    def __init__(self, name:str, layers:list, epsg: str, height: int = None, width: int = None):
         self.name = name
-        self.base_path = dotenv_values(".env")["FILE_PATH"]
-        self.scale = scale
+        self.layers = layers
+        self.height = height
+        self.width = width
         self.epsg = epsg
-        self.data_path = join(self.base_path, self.name)
         self.gdfs = self.get_gdfs()
         self.minx, self.miny, self.maxx, self.maxy = self.get_bounds()
         self.dwg = self.init_drawing()
+        self.name=''
+        self.scale_x = 1
+        self.scale_y = 1
 
     def get_gdfs(self) -> list:
-        print(self.data_path)
+        gdfs = [{**d, 'gdf': d['gdf'].to_crs(self.epsg)} for d in self.layers]
+        '''
         gdfs = []
-        for file in listdir(self.data_path):
-            if file.endswith('.geojson'):
-                gdf = geopandas.read_file(join(self.data_path, file))
-                gdfs.append({
-                    'name': file.split('.')[0],
-                    'gdf': gdf.to_crs(f'epsg:{self.epsg}')
-                })
+        for file in self.layers:
+            gdfs.append({
+                'name': 'name',
+                'gdf': file.to_crs(f'epsg:{self.epsg}')
+            })
+        '''
         return gdfs
+
+    def clip(self, mask):
+        self.gdfs = [{**d, 'gdf': d['gdf'].clip(mask=mask)} for d in self.gdfs]
+        self.minx, self.miny, self.maxx, self.maxy = self.get_bounds()
 
     def get_bounds(self):
         minx = miny = maxx = maxy = 0
         for gdf in self.gdfs:
-            _minx, _miny, _maxx, _maxy = [x * self.scale for x in gdf['gdf'].total_bounds]
+            _minx, _miny, _maxx, _maxy = [x for x in gdf['gdf'].total_bounds]
             minx = _minx if _minx < minx else minx
             miny = _miny if _miny < miny else miny
             maxx = _maxx if _maxx > maxx else maxx
@@ -62,11 +45,16 @@ class Map:
         return [minx, miny, maxx, maxy]
 
     def get_viewbox(self):
-        return f'0 {0 - (self.maxy - self.miny)} {self.maxx - self.minx} {self.maxy - self.miny}'
+        if self.width and self.height:
+            self.scale_x = self.width / (self.maxx - self.minx)
+            self.scale_y = self.height / self.maxy - self.miny
+            return f'0 {0 - (self.maxy - self.miny)} {self.width} {self.height}'
+        else:
+            return f'0 {0 - (self.maxy - self.miny)} {self.maxx - self.minx} {self.maxy - self.miny}'
 
     def init_drawing(self):
         return svgwrite.Drawing(
-            f'{self.base_path}/{self.name}.svg',
+            f'file.svg',
             height='100%',
             width='100%',
             viewBox=self.get_viewbox(),
@@ -75,47 +63,58 @@ class Map:
 
     def trans(self, coord: float, type: str):
         if type == 'x':
-            m = (coord * self.scale) - self.minx
+            m = (coord) - self.minx
         if type == 'y':
-            m = 0 - (coord * self.scale - self.miny)
+            m = 0 - (coord - self.miny)
         return round(m, 4)
 
+    def simplify(self, polygon:shapely.Polygon):
+        coord_num = len(polygon.exterior.coords)
+        if coord_num < 30000:
+            return polygon
+        geometry = polygon
+        tolerance = 0
+        while coord_num > 30000:
+            tolerance += 100
+            geometry = geometry.simplify(tolerance)
+            coord_num = len(geometry.exterior.coords)
+        return geometry
 
-    def add_path(self, coords: list, lyr):
-        data = [f"{(self.trans(x, 'x'))} {self.trans(y, 'y')}" for x, y in coords]
+    def add_path(self, polygon: shapely.Polygon, lyr):
+        simplified_polygon = self.simplify(polygon)
+        coordinates = simplified_polygon.exterior.coords
+        data = [f"{(self.trans(x, 'x'))} {self.trans(y, 'y')}" for x, y in coordinates]
         path_data = "M " + " ".join(data) + " Z"
         path = self.dwg.path(
             d=path_data,
+            class_=self.name,
+            stroke_width='1',
         )
-        path.scale(self.scale)
         lyr.add(path)
 
     def add_layer(self, gdf):
         lyr = svgwrite.container.Group(class_=gdf['name'], id=gdf['name'])
+        self.name = gdf['name']
         for feature in gdf['gdf'].iterrows():
             if 'name' in feature[1]:
+                name = 'feature' if 'name' not in feature[1] else feature[1]['name']
                 group = svgwrite.container.Group(id=name.replace(' ', '-'))
             else:
                 group = svgwrite.container.Group()
-            name = 'feature' if 'name' not in feature[1] else feature[1]['name']
             print(feature)
             geometry = feature[1]['geometry']
             if geometry.geom_type == 'Polygon':
-                coordinates = geometry.exterior.coords
-                self.add_path(coordinates, group)
+                self.add_path(geometry, group)
             if geometry.geom_type == 'MultiPolygon':
-                coordinates_list = [list(x.exterior.coords) for x in geometry.geoms]
-                for coordinates in coordinates_list:
-                    self.add_path(coordinates, group)
+                for geom in geometry.geoms:
+                    self.add_path(geom, group)
             lyr.add(group)
         self.dwg.add(lyr)
 
-    def save(self):
+    def save(self, filename: str):
+        with open('map.css') as f:
+            css = f.read()
         for gdf in self.gdfs:
             self.add_layer(gdf)
-        self.dwg.embed_stylesheet(style)
-        self.dwg.save()
-
-#map = Map('africa', epsg='esri:102022')
-map = Map('north-america')
-map.save()
+        self.dwg.embed_stylesheet(css)
+        self.dwg.saveas(filename=filename)
